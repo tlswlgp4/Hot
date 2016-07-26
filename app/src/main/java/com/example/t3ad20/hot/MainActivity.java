0eeb117d2372cef515b0a0c786479ab5747553ba
tlswlgp4 <tlswlgp4@naver.com>
2016-07-26 14:37:07 +0900

    java

---
+++ app/src/main/java/com/example/t3ad20/hot/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.t3ad20.hot;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
