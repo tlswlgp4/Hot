4ae3ec3c496b55febb7a94d70e37632cfb06e30c
tlswlgp4 <tlswlgp4@naver.com>
2016-07-27 16:13:40 +0900

    final

--- app/src/main/java/com/example/t3ad20/hot/MainActivity.java
+++ app/src/main/java/com/example/t3ad20/hot/MainActivity.java
@@ -2,41 +2,63 @@ package com.example.t3ad20.hot;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("select pic");
+        setTitle("명화 선호도 투표");
 
-        final int voteCount[] = new int[9];
-        for (int i = 0; i < 9; i++)
-            voteCount[i] = 0;
+        // 9개의 이미지 버튼 객체배열
         ImageView image[] = new ImageView[9];
+        // 9개의 이미지버튼 ID 배열
         Integer imageId[] = { R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5,
                 R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9 };
 
-        final String imgName[] = { "img1", "img2", "img3", "img4", "img5",
-                "img6", "img7", "img8", "img9" };
+        final String imgName[] = { "독서하는 소녀", "꽃장식 모자 소녀", "부채를 든 소녀",
+                "이레느깡 단 베르양", "잠자는 소녀", "테라스의 두 자매", "피아노 레슨", "피아노 앞의 소녀들",
+                "해변에서" };
 
         for (int i = 0; i < imageId.length; i++) {
-            final int index;
+            final int index; // 주의! 꼭 필요함..
             index = i;
             image[index] = (ImageView) findViewById(imageId[index]);
             image[index].setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     // TODO Auto-generated method stub
-                    voteCount[index]++;
+                    // 투표수 증가.
+                    JSONObject obj = new JSONObject();
+                    try {
+                        obj.put("url", "http://52.196.183.162:3000/insertData");
+                        obj.put("imageName", imgName[index]);
+                    }
+                    catch(Exception e) {
+                        Log.d("###", e.toString());
+                    }
+
+                    new HTTPAsyncTask().execute(obj);
                     Toast.makeText(getApplicationContext(),
-                            imgName[index] + ": 총 " + voteCount[index] + " 표",
+                            imgName[index] + "득표",
                             Toast.LENGTH_SHORT).show();
                 }
             });
@@ -47,12 +69,78 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),
                         ResultActivity.class);
-                intent.putExtra("VoteCount", voteCount);
-                intent.putExtra("ImageName", imgName);
                 startActivity(intent);
             }
         });
 
     }
+    private class HTTPAsyncTask extends AsyncTask<JSONObject, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(JSONObject... obj) {
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return HttpPost(obj[0]);
+            }
+            catch(Exception e) {
+                Log.d("###", e.toString());
+            }
+            return obj[0];
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(JSONObject obj) {
+            try {
+//                JSONObject tmp = new JSONObject(obj.get("result"));
+                Log.d("###", (String) obj.get("result"));
+            }
+            catch(Exception e) {
+                Log.d("###", e.toString());
+            }
+        }
+    }
 
+    private JSONObject HttpPost(JSONObject obj) throws IOException {
+        try {
+            InputStream inputStream = null;
+            URL url = new URL((String)obj.get("url"));
+
+            // create HttpURLConnection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // make PUT request to the given URL
+            conn.setRequestMethod("POST");
+            conn.connect();
+
+            OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+            out.write(obj.toString());
+            out.close();
+
+            // receive response as inputStream
+            inputStream = conn.getInputStream();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                obj.put("result", convertInputStreamToString(inputStream));
+            else
+                obj.put("result", "Did not work!");
+
+            return obj;
+        }
+        catch(Exception e) {
+            Log.d("###", e.toString());
+        }
+        return obj;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
 }
--- app/src/main/java/com/example/t3ad20/hot/ResultActivity.java
+++ app/src/main/java/com/example/t3ad20/hot/ResultActivity.java
@@ -1,15 +1,23 @@
 package com.example.t3ad20.hot;
 
-/**
- * Created by T3AD20 on 2016-07-26.
- */
 import android.app.Activity;
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RatingBar;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by jwjin on 7/26/16.
@@ -22,29 +30,17 @@ public class ResultActivity extends Activity {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result);
-        setTitle("Result activity");
+        setTitle("투표 결과");
 
-        Intent intent = getIntent();
-        int[] voteResult = intent.getIntArrayExtra("VoteCount");
-        String[] imageName = intent.getStringArrayExtra("ImageName");
-
-        TextView tv[] = new TextView[imageName.length];
-        RatingBar rbar[] = new RatingBar[imageName.length];
-
-        Integer tvID[] = { R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5,
-                R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9 };
-        Integer rbarID[] = { R.id.rbar1, R.id.rbar2, R.id.rbar3, R.id.rbar4,
-                R.id.rbar5, R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9 };
-
-        for (int i = 0; i < voteResult.length; i++) {
-            tv[i] = (TextView) findViewById(tvID[i]);
-            rbar[i] = (RatingBar) findViewById(rbarID[i]);
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("url", "http://52.78.90.34:3000/findData");
+        }
+        catch(Exception e) {
+            Log.d("###", e.toString());
         }
 
-        for (int i = 0; i < voteResult.length; i++) {
-            tv[i].setText(imageName[i]);
-            rbar[i].setRating((float) voteResult[i]);
-        }
+        new HTTPAsyncTask().execute(obj);
 
         Button btnReturn = (Button) findViewById(R.id.btnReturn);
         btnReturn.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +49,98 @@ public class ResultActivity extends Activity {
 
             }
         });
+
+    }
+
+    private class HTTPAsyncTask extends AsyncTask<JSONObject, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(JSONObject... obj) {
+            Log.d("###", "RUN AsyncTask");
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return HttpPost(obj[0]);
+            }
+            catch(Exception e) {
+                Log.d("###", e.toString());
+                return obj[0];
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(JSONObject obj) {
+            try {
+                JSONObject tmpObj = new JSONObject((String) obj.get("result"));
+                JSONArray tmpArr = tmpObj.getJSONArray("array");
+                Log.d("### result", tmpArr.toString());
+
+                //                // 9개의 TextView, RatingBar 객체배열
+                TextView tv[] = new TextView[tmpArr.length()];
+                TextView tv_1[] = new TextView[tmpArr.length()];
+                //                // 9개의 TextView, RatingBar ID 배열
+                Integer tvID[] = {R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5,
+                        R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9};
+                Integer tvID_1[] = {R.id.tv1_1, R.id.tv2_1, R.id.tv3_1, R.id.tv4_1, R.id.tv5_1,
+                        R.id.tv6_1, R.id.tv7_1, R.id.tv8_1, R.id.tv9_1};
+
+                for (int i = 0; i < tmpArr.length(); i++) {
+                    System.out.println(tmpArr.getJSONObject(i).get("imageName").toString());
+                    System.out.println(tmpArr.getJSONObject(i).get("count").toString());
+
+                    tv[i] = (TextView) findViewById(tvID[i]);
+                    tv[i].setText(tmpArr.getJSONObject(i).get("imageName").toString());
+                    tv_1[i] = (TextView) findViewById(tvID_1[i]);
+                    tv_1[i].setText(tmpArr.getJSONObject(i).get("count").toString());
+
+                }
+            }
+            catch(Exception e) {
+                Log.d("###", e.toString());
+            }
+        }
+    }
+
+    private JSONObject HttpPost(JSONObject obj) throws IOException {
+        try {
+            InputStream inputStream = null;
+            URL url = new URL((String)obj.get("url"));
+
+            // create HttpURLConnection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // make PUT request to the given URL
+            conn.setRequestMethod("POST");
+            conn.connect();
+
+            OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+            out.write(obj.toString());
+            out.close();
+
+            // receive response as inputStream
+            inputStream = conn.getInputStream();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                obj.put("result", convertInputStreamToString(inputStream));
+            else
+                obj.put("result", "Did not work!");
+
+            return obj;
+        }
+        catch(Exception e) {
+            Log.d("###", e.toString());
+        }
+        return obj;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
 
     }
 }
