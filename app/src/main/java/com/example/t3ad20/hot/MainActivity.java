84a9f94810d02c494870ef6edb742c5660c75a1d
tlswlgp4 <tlswlgp4@naver.com>
2016-07-27 13:38:44 +0900

    vote

--- app/src/main/java/com/example/t3ad20/hot/MainActivity.java
+++ app/src/main/java/com/example/t3ad20/hot/MainActivity.java
@@ -1,19 +1,58 @@
 package com.example.t3ad20.hot;
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("select pic");
 
+        final int voteCount[] = new int[9];
+        for (int i = 0; i < 9; i++)
+            voteCount[i] = 0;
+        ImageView image[] = new ImageView[9];
+        Integer imageId[] = { R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5,
+                R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9 };
 
+        final String imgName[] = { "img1", "img2", "img3", "img4", "img5",
+                "img6", "img7", "img8", "img9" };
 
+        for (int i = 0; i < imageId.length; i++) {
+            final int index;
+            index = i;
+            image[index] = (ImageView) findViewById(imageId[index]);
+            image[index].setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(),
+                            imgName[index] + ": 총 " + voteCount[index] + " 표",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        Button btnFinish = (Button) findViewById(R.id.btnResult);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        ResultActivity.class);
+                intent.putExtra("VoteCount", voteCount);
+                intent.putExtra("ImageName", imgName);
+                startActivity(intent);
+            }
+        });
 
     }
+
 }
